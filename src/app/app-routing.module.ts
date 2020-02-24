8b7577c5ea671bfdcb2512cbbd27112c03ac0b8a
Amy Troppe <amy@medology.com>
2020-02-23 21:05:20 -0600

    add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { DynamicViewComponent } from './dynamic-view/dynamic-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'TDF INTAKE'
+    }
+  },
+  {
+    path: 'view',
+    component: DynamicViewComponent,
+    data: {
+      title: 'TDF INTAKE'
+    }
+  },
+  // {
+  //   path: 'client-info',
+  //   component: ClientInfoComponent
+  // },
+  // {
+  //   path: 'confirmation',
+  //   component: ConfirmationComponent
+  // },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+    data: { title: 'Page Not Found' }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
